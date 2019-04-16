e6a8ae7aec9fd2254d841acf64ff65f4b3e9f3f0
Aronakamoto <aronakamoto@outlook.com>
2019-04-16 00:29:43 -0300

    Fix: Better implemention of UserService

--- api/services/UserService.ts
+++ api/services/UserService.ts
@@ -1,34 +1,87 @@
-import { UserModel } from "../models";
+import { BaseError, Service, ServiceOptions } from 'ts-framework-common';
+import { UserModel } from '../models';
+import { getRepository } from 'typeorm';
 
-/**
- * Class UserService
- *
- * @description Blockchain Insper SDK UserService method store to create an User.
- */
+export interface UserServiceOptions extends ServiceOptions {
+}
 
-export default class UserService {
-  public static async storeUser(firstName: string,
-    lastName: string, email: string, password: string): Promise<UserModel> {
+export default class UserService extends Service {
+  protected static instance: UserService;
+  public options: UserServiceOptions;
+
+  constructor(options: UserServiceOptions) {
+    super(options);
+  }
+
+  public static getInstance(options: UserServiceOptions) {
+    if (!this.instance) {
+      this.instance = new UserService({});
+    }
+    return this.instance;
+  }
+
+  public static initialize(options: UserServiceOptions) {
+    const service = new UserService(options);
+
+    if(!this.instance) {
+      this.instance = service;
+    }
+
+    return service;
+  }
+
+  public async showUser(id: string): Promise<UserModel> {
     try {
-      const user = await UserModel.create({
+      const repository = getRepository(UserModel);
+      const user = await repository.findOne(id);
+      return user;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  public async storeUser(firstName: string,
+    lastName: string, 
+    email: string,
+    password: string): Promise<UserModel> {
+    try {
+      const repository = getRepository(UserModel);
+      const user = await repository.insert({
         firstName,
         lastName,
         email,
         password
-      }).save();
-
-      return user;
-    } catch (e) {
-      console.error(e)
+      })
+      const userdb = await this.showUser(user.identifiers[0].id)
+      return userdb;
+    } catch (error) {
+      console.error(error)
     }
   }
 
-  public static async showUser(id: string): Promise<UserModel> {
+  public async showAllUser(): Promise<UserModel[]> {
     try {
-      const user = await UserModel.findOne(id);
-      return user;
-    } catch (e) {
-      console.error(e)
+      const repository = getRepository(UserModel);
+      const users = await repository.find()
+      return users
+    } catch (error) {
+      console.error(error)
     }
   }
-};
+  
+  async onMount(): Promise<void> {
+    this.logger.debug('Mounting UserService instance');
+  }
+
+  async onInit(): Promise<void> {
+    this.logger.debug('Initializing UserService instance');
+  }
+
+  async onReady(): Promise<void> {
+    this.logger.info('UserService initialized successfully');
+  }
+
+  async onUnmount(): Promise<void> {
+    this.logger.debug('Unmounting UserService instance');
+  }
+}
